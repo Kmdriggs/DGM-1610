6715e6fda3c9b65e936155343931927807120cfd
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-02-04 15:17:42 -0700

    Conventions and Syntax
    testing comments as well

---
+++ ConventionsAndSyntax.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConventionsAndSyntax : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(transform.position.x);
+
+        //Testing comments
+
+        if(transform.position.y <=6f)
+        {
+            Debug.Log ("I'm falling");
+        }
+    }
+
+}
