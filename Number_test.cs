fee5ca7f315239e1c118974c5bce8fc0ab5865fe
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-02-04 15:05:39 -0700

    Variables and functions
    Here is the actual file. I'm not sure what happened yesterday with Git.

---
+++ Number_test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Number_test : MonoBehaviour
+{
+    int myInt = 6;
+
+
+    void Start ()
+    {   
+        myInt = MultiplyByTwo(myInt)
+        Debug.Log (myInt)
+    }
+
+
+    int MultiplyByTwo (int number)
+    {
+        int ret;
+        ret = number * 2;
+        return ret;
+    }
+}
