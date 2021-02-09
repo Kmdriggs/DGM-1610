52f1a1b9edba153c2f4efe425137ecc49ab5a42c
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-02-08 23:49:55 -0700

    Variables and functions practice
    practicing coding

---
+++ Number_Practice.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Number_Practice : MonoBehaviour
+{
+    int myInt = 10;
+
+
+    void Start()
+    {
+        myInt = MultiplyByThree(myInt);
+        Debug.Log (myInt);
+    }
+
+
+    int MyltiplyByThree (int number)
+    {
+        int ret;
+        ret = number * 3;
+        return ret;
+    }
+
+}
