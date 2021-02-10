a336a6e36b09b63e524310126b572274e5f9743e
Kimberly Driggs <kmdriggs@gmail.com>
2021-02-10 10:58:42 -0700

    projects

---
+++ Project 1/Assets/Counter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+  public int creditValue = 2;
+  public IntData numberCount;
+  public Text counterText;
+
+
+  void OnTriggerEnter2D
+  {
+      numberCount.value += creditValue;
+      counterText.text = numberCount.value.ToString();
+      gameObject.SetActive(false);
+
+  }
+}
---
+++ Project 1/Assets/outdated.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEmgine.UI;
+
+public class outdated : MonoBehaviour
+{
+    public Text myText; //use this instead of GUItext 
+    public Image myTexture; //use instead of GUITexture
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
