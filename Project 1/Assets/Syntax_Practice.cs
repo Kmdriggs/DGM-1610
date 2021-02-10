9f5febb28a9622db45ab7506d7c302d3ecae6c07
Kimberly Driggs <kmdriggs@gmail.com>
2021-02-09 23:33:05 -0700

    test

---
+++ Project 1/Assets/Syntax_Practice.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections;
+using UnityEngine;
+
+public class Syntax_Practice : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(transform.position.y);
+
+        if (transform.position.x <= 10f)
+        {
+            Debug.Log("I'm falling");
+        }
+        
+    }
+    //Don't forget to check if all curly brackets have an end
+}
