8f35ca78391896bdcac563c9fa30681fecb09936
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-02-11 20:14:25 -0700

    If statements
    testing if statements using temperatures

---
+++ Temperature.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temperature : MonoBehaviour
+{
+   float roomTemperature = 75.5f;
+   float hotLimitTemperature = 80.0f;
+   float coldLimitTemperature = 60.0f;
+
+   void Update
+   {
+       if(Input.GetKeyDown(KeyCode.Space))
+            TemperatureTest();
+
+        roomTemperature -= Time.deltaTime * 5f;
+   }
+
+
+   void TemperatureTest ()
+   {
+       //if room temperature is too hot
+       if(roomTemperature > hotLimitTemperature
+       {
+           //place this
+           print("It's too hot");
+       }
+
+       //if room temperature is too cold
+       else if(roomTemperature < coldLimitTemperature)
+       {
+           //place this
+           print("It's too cold");
+       }
+
+       //if room temperature is perfect 
+       else
+       {
+           //place this
+           print("Everything is fine");
+       }
+
+
+   }
+
+
+}
