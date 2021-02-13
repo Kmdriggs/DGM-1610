5a7b3bc9a8243dced9c7883a56c4ceae75344103
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-02-12 21:42:03 -0700

    ForLoop
    trying out forloops

---
+++ Loops.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour
+{
+    int numEnemies = 5
+
+    void Start ()
+    {
+        for(int i = 0; i < numEnemies; i++)
+        {
+            Debug.Log("Enemy number: " + i);
+        }
+    }
+}
