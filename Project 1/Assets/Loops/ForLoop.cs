709adca213157cd1acb7a50f1f4957838d1aad51
Kimberly Driggs <kmdriggs@gmail.com>
2021-02-15 22:54:55 -0700

    Loops
    trying out different loops

---
+++ Project 1/Assets/Loops/ForLoop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoop : MonoBehaviour
+{
+    int SodasInFridge = 2
+
+
+    void Start()
+    {
+        while (SodasInFridge > 0)
+        {
+            Debug.Log("I want a soda"):
+            SodasInFridge--;
+        }
+    }
+}
---
+++ Project 1/Assets/Loops/RealForLoop.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealForLoop : MonoBehaviour
+{
+    int numEnemies = 5
+
+
+    void Start()
+    {
+        for (int i = 0; i < numEnemies; i++)
+        {
+            Debug.Log("Enemy Number: " + i); 
+        }
+    }
+}
