b9c4aef10f8de22b0c9c1885d453362ea1e785a7
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-02-18 19:46:47 -0700

    Practice Loops

---
+++ DoLoopPractice.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoLoopPractice : MonoBehaviour
+{
+    void Start()
+    {
+        bool shouldContinue = false;
+
+        do
+        {
+            print("Hello player");
+        } while (shouldContinue == true);
+    }
+}
---
+++ LoopsPractice.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopsPractice : MonoBehaviour
+{
+    int fishInPond = 4;
+
+
+    void Start ()
+    {
+        while(fishInPond > 0)
+        {
+            Debug.Log ("I found a fish!");
+            fishInPond--;
+
+        }
+    }
+}
