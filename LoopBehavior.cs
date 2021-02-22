8ce81c53aabbf843f8beef9e0d95e84f122c4724
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-02-22 10:45:14 -0700

    Practicing Loops

---
+++ LoopBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopBehavior : MonoBehaviour
+{
+    public int number, total;
+
+    public string[] cars;
+    public Weapon 
+
+
+    void Start()
+    {
+        for (int i = 0; i < number; i++)
+        {
+            //this is easier than typing "total =+ i;" 10 times
+            total = i;
+            print(total);
+
+        }
+
+        foreach (var car:string in cars)
+        {
+            print("I have a " + car);
+        }
+
+        foreach (var weapon in weapons)
+        {
+            print("You have my " + weapon.name);
+        }
+
+        
+    }
+
+    
+}
