0cef89d9d107277afb31845c0bf6db72bd8bd722
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-02-24 10:51:07 -0700

    Countdown behavoir
    understanding how to use while loops and use yield

---
+++ CountDownBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CountDownBehavior : MonoBehaviour
+{
+
+    public int number = 3;
+    public float holdTime = 1f;
+    public Text countDownText;
+
+
+    private WaitForSeconds wfsOBJ;
+
+
+    IEnumerator Start()
+    {
+        wfsOBJ = new WaitForSeconds(holdTime);
+
+        while (number > 0)
+        {
+            countDownText.text = Number.ToString();
+            number--;
+            yield return wfsOBJ;
+        }
+
+        countDownText.text = "Go!";
+
+        yield return wfsOBJ;
+
+        countDownText.text = null;
+    }
+}
