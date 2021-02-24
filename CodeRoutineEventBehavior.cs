02512d6b3f81104afd2de62c3759d3082825d087
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-02-24 10:51:51 -0700

    Code routine events behavior
    using while loop and yield

---
+++ CodeRoutineEventBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class CodeRoutineEventBehavior : MonoBehaviour
+{
+    public UnityEvent startEvent;
+    public int number, maxNumber;
+    public float holdTime = 1f;
+    private WaitForSeconds wfsOBJ;
+
+    IEnumerator Start()
+    {
+        wfsOBJ = new WaitForSeconds(holdTime);
+
+        while (number < maxNumber)
+        {
+            startEvent. Invoke();
+            yield return wfsOBJ;
+
+        }
+    }
+}
