d92a74562bf7382afc092f14311322bc4737ea95
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-02-23 18:00:26 -0700

    Practicing Loops
    for and foreach loops

---
+++ Loop.PracticeEm.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loop.PracticeEm : MonoBehaviour
+{
+    public string[] sodas;
+    int Enemies = 5
+
+
+    void Start ()
+    {
+        for(int i = 0; i < Enemies; i++)
+        {
+            Debug.Log("Enemies Defeated: " + i);
+        }
+
+        foreach(var soda:string in sodas)
+        {
+            print("I have " + soda);
+        }
+    }
+}
