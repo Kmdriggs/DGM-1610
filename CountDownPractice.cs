778021b951fcc989002fdafbccaba17392cdcfd9
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-02-25 11:16:45 -0700

    Countdown practice
    Using while loops and yield returns

---
+++ CountDownPractice.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDownPractice : MonoBehaviour
+{
+    public int number = 10;
+    public float holdTime = 1f;
+    public Text countDownText;
+
+    private WaitSecond wsOBJ;
+
+
+
+    IEnumerator Start()
+    {
+        wsOBJ = new WaitSecond(holdTime);
+
+        while (number > 0)
+        {
+            countDownText.text = Number.ToString();
+            number--;
+            //put yield to prevent computer from crashing
+            yield return wsOBJ;
+        }
+
+        countDownText.text = "End!";
+
+       //put yield to prevent computer from crashing
+        yield return wsOBJ;
+
+        countDownText.text = null;
+    }
