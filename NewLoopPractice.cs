4a614eaea6c10850091fc075724ddf5b3721632c
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-02-26 11:04:00 -0700

    Practice Loops
    Practicing loops using player names

---
+++ NewLoopPractice.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewLoopPractice : MonoBehaviour
+{
+    public string[] playerNames;
+
+
+    private IEnumerator Start()
+    {
+        //use var instead of int
+        for (var playerName string in playerNames)
+        {
+            print(playerNames);
+        }
+
+        while (true)
+        {
+            //wait 1 second
+            yield return new waitForSeconds(1);
+            print("Hello");
+        }
+    }
+}
