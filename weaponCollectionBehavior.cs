3453cca147f035ed6a2811a2915e29d8bfd4ca0b
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-02-26 11:02:58 -0700

    Weapons
    3 different codes. One practicing lists and for each loops

---
+++ weaponCollectionBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponCollectionBehavior : MonoBehaviour
+{
+    public weapon weaponsObj;
+    public WeaponCollection weaponCollectionObj;
+
+    private void Start()
+    {
+        foreach (var obj Weapon in weaponCollectionObj.weaponList)
+        {
+            if (weapon == weaponObj)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        weaponCollectionObj.weaponsList.Add(weaponObj);
+        gameObject.SetActive(false);
+
+
+    }
+
+}
---
+++ weaponsCollection.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class weaponCollection : ScriptableObject
+{
+    public List<Weapon> weaponsList;
+}
