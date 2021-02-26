4871f8146cf254892ea3268a27c007b40c2b468a
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-02-26 11:04:42 -0700

    Data Points

---
+++ DataPoints.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataPoints : ScriptableObject
+{
+    public float up, down, side;
+
+    pub;ic BaseClass newBseClass = new BaseClass();
+
+    public DataPoints Output()
+    {
+        var other BaseClass - newBaseClass;
+        var obj DataPoints - this; 
+        obj.down = 5f;
+        return obj;
+    }
+}
+    
