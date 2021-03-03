898614aa2ed11aaceb601b37931dbf4a76406e47
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-03-03 15:03:18 -0700

    Active Objects
    Practicing active objects and understanding how to use them

---
+++ CheckState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckState : MonoBehaviour
+{
+    public GmeObject myObj;
+
+
+    void Start ()
+    {
+        Debug.Log("Active Self: " + myObj.activeSelf);
+        Debug.Log("Active in Class" + myObj.activeInClass)
+    }
+}
