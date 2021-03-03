1d1a74db7cc730f6a85b162ba783d5f03c8c5fe4
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-03-03 15:08:07 -0700

    Transform functions
    practicing how to use transform functions

---
+++ TransformFunctions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformFunctions : MonoBehaviour
+{
+    public float moveSpeed = 20f;
+    public float turnSpeed = 60f;
+
+
+    void Update ()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.forward *movespeed *Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+}
