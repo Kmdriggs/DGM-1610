81f88777e23db6416011f09d417660f6d8bb86bc
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-03-05 22:18:46 -0700

    Axis
    Learning and practicing how to use axis

---
+++ AxisRawTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxisRawTest : MonoBehaviour
+{
+    public float range;
+    public GUI textOutput;
+
+
+    void Update ()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float xPos = h * range;
+
+
+        transform.position = new Vector3(xPos, 4f, 0);
+        textOutput.text = "ValueReturned: "+h.ToString("F2");
+    }
+}
---
+++ AxisTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxisTest : MonoBehaviour
+{
+    public float range;
+    public GUIText textOutput;
+
+
+    void Update ()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float xPos = h * range;
+
+
+        transform.position = new Vector3(xPos, 4f, 0);
+        textOutput.text = "Vaule Returned: "+h.ToString("F2");
+    }
+}
