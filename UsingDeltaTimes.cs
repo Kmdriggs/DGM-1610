e9f132c02e677d5f002db422cff0647a2833c551
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-03-15 10:28:27 -0600

    Delta Times
    trying out delta times

---
+++ UsingDeltaTimes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsingDeltaTimes : MonoBehaviour
+{
+
+    public float speed = 6f;
+    public float countdown = 4.0f;
+
+
+    void Update ()
+    {
+        countdown -= Time.deltaTime;
+        if(countdown <= 0.0f)
+            light.enable = true;
+        
+        if(Input.GEtKey(KeyCode.RightArrow))
+            transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+    }
+}
