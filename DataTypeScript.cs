866f203ebfa4fe7a0fdbb77cd5db1d5515592a9d
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-03-15 10:31:33 -0600

    Data Types
    trying out different data types

---
+++ DataTypeScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataTypeScript : MonoBehaviour
+{
+    void Start ()
+    {
+        //value type variable
+        Vector3 pos = transform.position;
+        pos = new Vector3(0 6, 0);
+
+        //refernce type variable
+        Transform tran = transform;
+        tran.position = new Vector3(0, 6, 0);
+    }
+}
