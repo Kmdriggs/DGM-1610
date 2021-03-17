ac14257e85b3a45df0f464a59f53c229d50ffa68
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-03-16 21:37:13 -0600

    Get Components
    understanding how to use components

---
+++ UsingOtherComponents.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsingOtherComponents : MonoBehaviour
+{
+    public GameObject otherGameObject;
+
+
+    private AnotherScript anotherScript;
+    private YetAnotherScript yetAnotherScript;
+    private BoxCollider boxCol;
+
+
+    void Awake ()
+    {
+        anotherScript = GetComponent<AnotherScript>();
+        yetAnotherScript = otherGameObject.GetComponent<YetAnotherScript>();
+        boxCol = otherGameObject.GetComponent<BoxCollider>();
+    }
+
+
+    void Start()
+    {
+        boxCol.size = new Vector3(2,2,2,);
+        Debug.Log("The player's sore is " + anotherScript.playerScore);
+        Debug.Log("The player has died " + yetAnotherScript.numberOfPlayers + " times");
+    }
+}
