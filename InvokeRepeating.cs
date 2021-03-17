824434d4226f59b84e56b9beebebb1abd4872c5a
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-03-17 10:18:43 -0600

    Invoke

---
+++ InvokeRepeating.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvokeRepeating : MonoBehaviour
+{
+    public GameObject target;
+
+
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", 2, 1);
+    }
+
+
+    void SpawnObject()
+    {
+        float x = Random.Range(-3.0f, 3.0f);
+        float z = Random.Range(-3.0f, 3.0f);
+        Instantiate(target, new Vector3(x, 3, z), Quaternion.identity);
+    }
+}
---
+++ InvokeScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvokeScript : MonoBehaviour
+{
+    public GameObject target;
+
+
+    void Start()
+    {
+        Invoke ("SpawnObject", 2);
+    }
+
+
+    void SpawnObject()
+    {
+        Instantiate(target, new Vector3(0, 3, 0, Quaternion.identity);
+    }
+}
