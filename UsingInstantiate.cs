4f77fdfb550c3eb5daa1e91f2c35d8c5dfb66758
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-03-16 21:36:35 -0600

    Instantiate
    understanding how to use instantiate

---
+++ UsingInstantiate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsingInstantiate : MonoBehaviour
+{
+    //instantiate: used to clone objects
+    public Rigidbody rocketPrefab;
+    public Transform barrelEnd;
+
+
+    void Update ()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody rocketInstance;
+            rocketInstance = Instantiate(rocketPrefab, barrelEnd.position, barrelEnd.rotation) as Rigidbody;
+            rocketInstance.AddForece(barrelEnd.forward * 6000);
+        }
+    }
+}
