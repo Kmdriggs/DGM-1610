20767ae3ffc7a17b307f5d710cbbeb4e0a7a5ee2
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-03-23 23:37:46 -0600

    Challenge 1 Scripts

---
+++ spinPropellorX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spinPropellorX : MonoBehaviour
+{
+    private float propellorSpeed = 1000;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, propellorSpeed * Time.deltaTime);
+        
+    }
+}
