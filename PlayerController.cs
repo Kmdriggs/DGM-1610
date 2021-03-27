9d11c273482d9f83c115a9f4cbd89b330bdb9568
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-03-27 12:57:10 -0600

    Updated scripts

--- PlayerController.cs
+++ PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 12;
 
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +27,12 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //launch a projectile from player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
 
 
