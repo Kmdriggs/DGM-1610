7b70b352d3a75192e40ed6edf9d8dcb36a39abd0
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-03-31 23:39:51 -0600

    Prototype 3 player controller script

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,39 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float horizontalInput;
-    public float speed = 10.0f;
-    public float xRange = 12;
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
 
-    public GameObject projectilePrefab;
-
-    // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < -xRange)
-        {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
-        }
-
-        if (transform.position.x > xRange)
-        {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-        }
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            //launch a projectile from player
-            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
-
-
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right *horizontalInput * Time.deltaTime * speed);
     }
 }
