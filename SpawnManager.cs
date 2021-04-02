571c87e1dfed720e023ed6c233d414380c98f20f
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-04-01 23:49:00 -0600

    Spawn Manager Script for Create with Code 3

--- SpawnManager.cs
+++ SpawnManager.cs
@@ -4,27 +4,24 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject[] animalPrefabs;
-    private float spawnRangeX = 20;
-    private float spawnPosZ = 20;
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
-    private float spawnInterval = 1.5f;
-
+    private float repeatRate = 2;
 
     void Start()
     {
-        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
+    // Update is called once per frame
     void Update()
     {
         
     }
 
-    void SpawnRandomAnimal()
+    void SpawnObstacle ()
     {
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 }
