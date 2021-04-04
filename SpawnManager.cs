7850f128cd4933a53b293f29b2f86b5078c79fa8
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-04-03 23:58:36 -0600

    New Unit 3 Scripts
    Unit 3 unity file crashed and got deleted. These are the new replacement scripts.

--- SpawnManager.cs
+++ SpawnManager.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private Vector3 spawnPos = new Vector3(25, 0 , 0);
     private float startDelay = 2;
     private float repeatRate = 2;
 
+
+    // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+       InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
@@ -22,6 +24,6 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnObstacle ()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        Instantiate(objectPrefab, spawnPos, obstaclePrefab.trnsform.rotation);
     }
 }
