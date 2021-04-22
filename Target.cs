6e2ec4c8d4f5b061cde7f20e4df39abce03a0b2b
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-04-21 23:57:45 -0600

    Target Script 5

---
+++ Target.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForeceMode.Impulse);
+        targetRb.AddTorque(Random.Range(-10, 10), Random.Rance(-10, 10), Random.Range(-10, 10), ForeceMode.Impulse);
+        transform.position = new Vector3(Random.Range(-4, 4), -6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
