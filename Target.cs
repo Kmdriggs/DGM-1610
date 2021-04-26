219fa604201b21bba92f58a4af44c99c48ced68a
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-04-25 22:06:22 -0600

    Updated Target and GameManager Scripts
    5

--- Target.cs
+++ Target.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private GameManager gameManager;
+
+    public int pointValue;
+    public ParticleSystem explosionParticle;
 
     private float minSpeed = 12;
     private float maxSpeed =16;
@@ -19,6 +23,7 @@ public class Target : MonoBehaviour
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     Vector3 RandomForce()
@@ -41,4 +46,17 @@ public class Target : MonoBehaviour
     {
         
     }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
 }
