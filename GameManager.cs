42c92496cd27252f63b14e77463b62908cc035cb
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-04-26 18:24:47 -0600

    Completed Scripts for Create with Code 5
    DifficultyButton, GameManager, and Target Scripts

--- GameManager.cs
+++ GameManager.cs
@@ -1,28 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public GameObject[] targets2;
-    private int score;
+    public GameObject titleScreen;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    private int score;
+
+    public bool isGameActive;
+    public Button restartButton;
 
     private float spawnRate = 1.0f;
     
+
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
+
     }
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
@@ -42,4 +48,25 @@ public class GameManager : MonoBehaviour
         scoreText.text = "Score: " + score;
     }
 
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate /= difficulty;
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
+    }
 }
