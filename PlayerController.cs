deab6d17552fac20c76abc7c24424a8aac3240ce
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-04-27 23:57:51 -0600

    New playerController script for final project

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 25.0f;
-    private float zBound = 6;
     private Rigidbody playerRb;
+    private float speed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,47 +16,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MovePlayer();
-        ConstrainPlayerPosition();
-
+        playerMovement();
     }
-    //Move player by arrow keys
-    void MovePlayer()
+
+    //player movement
+    void playerMovement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-
-        playerRb.AddForce(Vector3.forward * speed * verticalInput);
         playerRb.AddForce(Vector3.right * speed * horizontalInput);
-    }
-
-    //prevent player from leaving screen
-    void ConstrainPlayerPosition()
-    {
-        if (transform.position.z < -zBound)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
-        }
-
-        if (transform.position.z > zBound)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
-        }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.CompareTag("Enemy"))
-        {
-            Debug.Log("Game Over");
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.CompareTag("Point"))
-        {
-            Destroy(other.gameObject);
-        }
     }
 }
