79ea05b0c0b5d5864bd25bb32e91efffedde4f8d
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-04-29 23:44:58 -0600

    Updated codes from create with code 1
    Understanding different types of updates (fixed and late), using serializedField, and understanding the use of const, static and readonly

---
+++ Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+    [SerializeField] protected const float speed = 20.0f;
+    [SerializeField] private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //moves car forward based on vertical input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //roates car based on horizontal input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
