fd76a1b733623d7bd32f377d32725ab1b9ad7542
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-05-03 16:16:29 -0600

    Practice Unity Events
    Trying Out Unity Events

---
+++ SpawnCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCube : MonoBehaviour
+{
+    [SerializeField] private GameObject pillar;
+
+    public void ActivatePillar()
+    {
+        pillar.SetActive(true);
+    }
+
+    public void ActivateSphere()
+    {
+        Debug.Log("Make Pillar Appear");
+    }
+}
---
+++ TriggerEvents.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvents : MonoBehaviour
+{
+    [SerializeField] private UnityEvent myTrigger;
+
+    void Start()
+    {
+        if(myTrigger == null)myTrigger = new UnityEvent();
+        myTrigger.AddListener(Ping);
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown && myTrigger != null)
+        {
+            myTrigger.Invoke();
+        }
+    }
+
+    void Ping()
+    {
+        Debug.Log("Ping");
+    }
+}
